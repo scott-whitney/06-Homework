37c037ff3b0f34dd735b0574ab4a0d93afc1053a
Minroe <scott.whitney334@gmail.com>
2020-03-30 21:48:36 -0700

    second Commit

--- Script.js
+++ Script.js
@@ -2,14 +2,25 @@ $(document).ready(function(){
     var searchCity = '';
     $('#userBtn').on('click', appendCity);
     const d = new Date();
-
-    const dd = d.getDate();
-    const mm = d.getMonth()+1;
-    const yyyy = d.getFullYear();
-    // let totaldays = new Date(yyyy, mm, 0).getDate()
+    var dd = d.getDate();
+    var mm = d.getMonth()+1;
+    var yyyy = d.getFullYear();
     let output =
     (mm<10 ? '0' : '') + mm + '/' +
     (dd<10 ? '0' : '') + dd + '/' + yyyy
+    var monthy = (mm<10 ? '0' : '') + mm
+    var dayy = (dd<10 ? '0' : '') + dd
+    console.log(monthy);
+    console.log(dayy);
+    $('#Hider').hide();
+
+    var storedSearches = localStorage.getItem('Searches');
+    var pulledSearches = JSON.parse(storedSearches);
+
+
+
+    var recentSearches = [];
+    
 
 
     function cityWeather(clicked) {
@@ -20,10 +31,7 @@ $(document).ready(function(){
           url,
           method: 'GET'
         }).then(response => {
-          console.log(response);
-          console.log(response.main.temp)
-          console.log(response.main.humidity)
-          console.log(response.wind.speed)
+
           $('#currentCity').text(searchCity);
           $('#currentTemp').text(response.main.temp);
           $('#currentHumidity').text(response.main.humidity);
@@ -44,6 +52,45 @@ $(document).ready(function(){
             url,
             method: 'GET'
         }).then(response => {
+            day0 = new Date(d.getTime() + (1000 * 60 * 60 * 24));
+            var dd0 = day0.getDate();
+            var mm0 = day0.getMonth()+1;
+            var yyyy0 = day0.getFullYear();
+            let output0 =
+            (mm0<10 ? '0' : '') + mm0 + '/' +
+            (dd0<10 ? '0' : '') + dd0 + '/' + yyyy0
+
+            day1 = new Date(d.getTime() + (1000 * 60 * 60 * 48));
+            var dd1 = day1.getDate();
+            var mm1 = day1.getMonth()+1;
+            var yyyy1 = day1.getFullYear();
+            let output1 =
+            (mm1<10 ? '0' : '') + mm1 + '/' +
+            (dd1<10 ? '0' : '') + dd1 + '/' + yyyy1
+
+            day2 = new Date(d.getTime() + (1000 * 60 * 60 * 72));
+            var dd2 = day2.getDate();
+            var mm2 = day2.getMonth()+1;
+            var yyyy2 = day2.getFullYear();
+            let output2 =
+            (mm2<10 ? '0' : '') + mm2 + '/' +
+            (dd2<10 ? '0' : '') + dd2 + '/' + yyyy2
+
+            day3 = new Date(d.getTime() + (1000 * 60 * 60 * 96));
+            var dd3 = day3.getDate();
+            var mm3 = day3.getMonth()+1;
+            var yyyy3 = day3.getFullYear();
+            let output3 =
+            (mm3<10 ? '0' : '') + mm3 + '/' +
+            (dd3<10 ? '0' : '') + dd3 + '/' + yyyy3
+
+            day4 = new Date(d.getTime() + (1000 * 60 * 60 * 120));
+            var dd4 = day4.getDate();
+            var mm4 = day4.getMonth()+1;
+            var yyyy4 = day4.getFullYear();
+            let output4 =
+            (mm4<10 ? '0' : '') + mm4 + '/' +
+            (dd4<10 ? '0' : '') + dd4 + '/' + yyyy4
             console.log(response);
  
             $('#temp0').text("Temp:" + response.list[0].temp.day)
@@ -58,7 +105,21 @@ $(document).ready(function(){
             $('#humidity3').text("Humid:" + response.list[3].humidity + "%")
             $('#humidity4').text("Humid:" + response.list[4].humidity + "%")
 
+            $('#dater0').text(output0)
+            $('#dater1').text(output1)
+            $('#dater2').text(output2)
+            $('#dater3').text(output3)
+            $('#dater4').text(output4)
+
             $('#currentCity').text(clicked + ": " + output);
+            for (i=0; i < 5; i++){
+                // $(`#dater${i}`).text(monthy + "/" + dayy + "/" + yyyy)
+                if (response.list[i].temp.day > 80){
+                    $(`#day${i}`).attr('class', 'bg-danger')
+                } else if (response.list[i].temp.day < 80) {
+                    $(`#day${i}`).attr('class', 'bg-primary')
+                }
+            }
 
         })
     }
@@ -67,19 +128,52 @@ $(document).ready(function(){
 
     function appendCity() {
         searchCity = $('#citySearchValue').val();
-        console.log(searchCity);
-        const newBtn = $('<button>').attr('value', searchCity).attr('class', 'btn btn-primary').text(searchCity);
+        var newBtn = $('<button>').attr('value', searchCity).attr('class', 'btn btn-primary').text(searchCity);
         $('#savedCities').prepend(newBtn);
+        recentSearches.push(searchCity)
+
+
+
+        
+        recentString = JSON.stringify(recentSearches);
+        localStorage.setItem('Searches', recentString);
+ 
+
         newBtn.on('click', function(){
-            if($(this).is('button')){
-               citySelected = $(this).attr('value')
-               console.log(citySelected)
-                cityWeather(citySelected);
-                cityForecast(citySelected);
-            }
-        })
+            citySelecter = $(this).attr('value')
+            onClick(citySelecter);
+
+        });
         cityWeather(searchCity);
         cityForecast(searchCity);
+        reveal();
+    }
+    function onClick(arr){
+            
+            console.log(arr);
+            cityWeather(arr);
+            cityForecast(arr);
+            reveal();
+        
+
+    }
+
+    function reveal(){
+        $('#Hider').show();
+
+
+    }
+    if(pulledSearches) {
+        for(i=0; i<pulledSearches.length; i++){
+            console.log(pulledSearches[i])
+            var newBtn = $('<button>').attr('value', `${pulledSearches[i]}`).attr('class', 'btn btn-primary').text(pulledSearches[i])
+            $('#savedCities').prepend(newBtn)
+            $(newBtn).on('click', function(){
+                citySelected = $(this).attr('value')
+                onClick(citySelected)
+            });
+
+        }
     }
 
 
